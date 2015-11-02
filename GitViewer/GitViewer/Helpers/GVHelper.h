d431132bd8b6b4384eef9b4f87ec12322f0969d1
Max Tymchiy <Samback@ukr.net>
2015-11-02 21:34:55 +0200

    Add pod files and helper file

---
+++ GitViewer/GitViewer/Helpers/GVHelper.h
@@ -0,0 +1,16 @@
+//
+//  GVHelper.h
+//  GitViewer
+//
+//  Created by Max Tymchiy on 11/2/15.
+//  Copyright © 2015 Max Tymchiy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const kClientID;
+extern NSString *const kClientSecretID;
+
+@interface GVHelper : NSObject
+
+@end
---
+++ GitViewer/GitViewer/Helpers/GVHelper.m
@@ -0,0 +1,16 @@
+//
+//  GVHelper.m
+//  GitViewer
+//
+//  Created by Max Tymchiy on 11/2/15.
+//  Copyright © 2015 Max Tymchiy. All rights reserved.
+//
+
+#import "GVHelper.h"
+
+NSString *const kClientID = @"6ef19636b8f523ec532d";
+NSString *const kClientSecretID = @"e1ea12767019182c8db931a14e2ab9010f90aad8";
+
+@implementation GVHelper
+
+@end
